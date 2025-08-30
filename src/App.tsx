15c423ba5155efebed01b32ccb7cb2b31ee9942c
Rohit singh <rohitdev124421@gmail.com>
2025-08-30 21:39:00 +0530

    prep 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
+// task 1 complete
+
+// import React, { useState } from 'react'
+ 
+//  const App = () => {
+//   let [number , setNumber] = useState<number>(1)
+//     return (
+//      <div>
+//        <p>hello my name is rohit singh and i am {number} years old</p>
+// <button onClick={() => setNumber(number+1)}> add age</button>
+// <button onClick={() => setNumber(number-1)}> minus age</button>
+// <button onClick={() => setNumber(number = 1)}> reset age</button>
+//      </div>
+//    )
+//  }
+ 
+//  export default App
+// task 2
 import React, { useState } from "react";
 
-const App = () => {
-  const [isClicked, setIsClicked] = useState<boolean>(true);
+// Correct type
+type User = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+};
 
-  function handleClick() {
-    if (isClicked) {
-      console.log("click");
-    } else {
-      console.log("not click");
-    }
-    setIsClicked(!isClicked); // toggle state
-  }
+const App = () => {
+  // Initial value given
+  const [info, setInfo] = useState<User>({
+    name: "Rohit Singh",
+    age: 19,
+    isStudent: true,
+  });
+
+  // Function to update info
+  const updateUser = () => {
+    setInfo({
+      ...info, // spread old values
+      name: "Aman", // update name
+      age: info.age + 1, // increase age
+      isStudent: !info.isStudent, // toggle student status
+    });
+  };
 
   return (
     <div>
-      <h1>{isClicked ? "True hai" : "False hai"}</h1>
-      <button onClick={handleClick}>Click Me</button>
+      <p>Name = {info.name}</p>
+      <p>Age = {info.age}</p>
+      <p>Student = {info.isStudent ? "Yes" : "No"}</p>
+
+      <button onClick={updateUser}>Update User</button>
     </div>
   );
 };
